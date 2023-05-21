8852e547f3e5da87fe1ff08cd323ada97ab6a63e
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-05-21 10:56:52 +0700

    feat:header
    refs:DW1-3

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,18 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/src/app/layouts/base-layout/base-layout.component.ts
+++ angular/src/app/layouts/base-layout/base-layout.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { faUser } from "@fortawesome/free-solid-svg-icons"
 @Component({
   selector: 'app-base-layout',
   templateUrl: './base-layout.component.html',
   styleUrls: ['./base-layout.component.scss']
 })
 export class BaseLayoutComponent {
-
+  faUser = faUser
 }
