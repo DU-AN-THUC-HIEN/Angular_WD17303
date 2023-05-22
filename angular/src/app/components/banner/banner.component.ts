0737c1bbec58700d48d8a80e62023cc81eae40f1
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-05-22 13:29:52 +0700

    feat: Cập nhật banner

--- angular/src/app/components/banner/banner.component.ts
+++ angular/src/app/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -7,4 +8,3 @@ import { Component } from '@angular/core';
 export class BannerComponent {
 
 }
-
