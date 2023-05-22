c0da717cbfe72682d9e529ce68b289d8033a98bd
binhtrong4354t <saibinhtrong18122003@gmail.com>
2023-05-22 14:06:51 +0700

    feat: xây dựng base admin
    refs:DW1-7

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -15,11 +15,7 @@ const routes: Routes = [
   },
   {
     path: "admin",component: AdminLayoutComponent,
-  //   children: [
-  //     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  //     { path: 'dashboard', component: DashboardComponent },
-     
-  // ],
+  
     
   },
   {
