3d84ad95f9f650d51468f66fca4b1955f46563db
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-05-26 15:09:35 +0700

    feat: Hoàn thành api upload refs:DW1-24

--- API_Node/src/app.js
+++ API_Node/src/app.js
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 import cors from "cors";
 import router from "./routes/products";
 import routerUser from "./routes/users";
-import routerCategory from "./routes/categories"
+import routerCategory from "./routes/categories";
+import uploadRouter from "./routes/upload";
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api", router);
 app.use("/api", routerUser);
 app.use("/api", routerCategory);
+app.use("/api", uploadRouter);
+
 mongoose.connect("mongodb://127.0.0.1:27017/Angular_WD17303");
 
 export const viteNodeApp = app;
