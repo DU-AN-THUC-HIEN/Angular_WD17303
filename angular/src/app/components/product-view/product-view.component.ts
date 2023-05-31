93b59589144c89ff40e2095f16117eba552a314a
binhtrong4354t <saibinhtrong18122003@gmail.com>
2023-05-31 14:58:32 +0700

    feat: product-view
    refs: DW1-34

--- angular/src/app/components/product-view/product-view.component.ts
+++ angular/src/app/components/product-view/product-view.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { IProduct } from 'src/app/interface/product';
+import { ProductService } from 'src/app/services/product/product.service';
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
   styleUrls: ['./product-view.component.scss']
 })
 export class ProductViewComponent {
-
+  products: IProduct[] = [];
+  
+  constructor(
+    private productService: ProductService) {
+      
+  }
+  ngOnInit(){
+    this.productService.getProducts()
+    .subscribe((data : any) => this.products = data.docs)
+  }
+ 
 }
