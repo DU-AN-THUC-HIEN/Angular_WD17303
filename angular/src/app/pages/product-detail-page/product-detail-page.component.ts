fc31628cce24930b5f58378bf5e2bb9a90df2e02
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-05-31 15:16:16 +0700

    feat: Hoàn thành products detail - product detail - sản phẩm cùng loại refs: DW1-36

--- angular/src/app/pages/product-detail-page/product-detail-page.component.ts
+++ angular/src/app/pages/product-detail-page/product-detail-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ICategory } from 'src/app/interface/category';
+import { IProduct } from 'src/app/interface/product';
+import { CategoryService } from 'src/app/services/category/category.service';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-product-detail-page',
@@ -7,6 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailPageComponent {
   quantity: number = 1;
+  product !: IProduct
+  products: IProduct[] = [];
+  categories: ICategory[] = [];
+  constructor(
+    private categoryService: CategoryService,
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.route.paramMap.subscribe(params => {
+      const id = String(params.get('id'));
+      this.productService.getProductById(id).subscribe(product => {
+        this.product = product;
+      }, error => console.log(error.message)
+      )
+    })
+    this.productService.getProducts().subscribe((products: any) => {
+      this.products = products.docs.filter((product: IProduct) => product.categoryId === this.product.categoryId);
+      console.log(this.products);
+
+    })
+  }
+
 
   decreaseQuantity() {
     if (this.quantity > 1) {
