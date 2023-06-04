20e4ec4431c009782a296f5908ce8720fa789133
Duyy2707 <duy27072003@gmail.com>
2023-06-04 11:26:49 +0700

    feat: Hoàn Thành api comment - GetOne comment theo Id - Get comment theo productId(không hện được userId, giá trị = null) - Create, Update, Delete comment thành công theo Id refs: DW1-18

---
+++ API_Node/src/routes/comment.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { checkPermission } from "../middlewares/checkPermission";
+import { create, getCommentFromProduct, getOneComment, removeComment, updateComment } from "../controller/comment";
+
+const routerComment = express.Router();
+
+routerComment.get("/comment/:productId", getCommentFromProduct)
+routerComment.post("/comment/:id", getOneComment)
+routerComment.post("/comment", create)
+routerComment.put("/comment/:id", updateComment, checkPermission)
+routerComment.delete("/comment/:id", removeComment)
+
+export default routerComment;
