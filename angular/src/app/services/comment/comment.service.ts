be8672fbf199e13820293d4b10aacd926b9c0a75
Minh452003 <117347792+Minh452003@users.noreply.github.com>
2023-06-06 16:18:12 +0700

    feat: Hoàn thành add comment refs: DW1-47

--- angular/src/app/services/comment/comment.service.ts
+++ angular/src/app/services/comment/comment.service.ts
@@ -10,14 +10,14 @@ export class CommentService {
 
   constructor(private http: HttpClient) { }
 
-  addComment(comment: IComment): Observable<IComment> {
-    return this.http.post<IComment>(`http://localhost:8080/api/comment`, comment)
+  addComment(comment: any): Observable<IComment> {
+    return this.http.post<any>(`http://localhost:8080/api/comment`, comment)
   }
   removeComment(id: any): Observable<IComment> {
     return this.http.delete<IComment>(`http://localhost:8080/api/comment/${id}`)
   }
   getCommentById(id: any): Observable<IComment> {
-    return this.http.get<IComment>(`http://localhost:8080/api/comment/${id}`)
+    return this.http.get<IComment>(`http://localhost:8080/api/comment/${id}/detail`)
   }
   getCommentByProduct(productId: string): Observable<IComment[]> {
     return this.http.get<IComment[]>(`http://localhost:8080/api/comment/${productId}`)
