867d4f30684703977be606e699b05b81f61cb4b7
[hieutqph21650] <[hieutqph21650@fpt.edu.vn]>
2023-06-15 09:55:49 +0700

    feat: Hủy đơn hàng _ fix api refs:DW1-76

--- angular/src/app/pages/bill/bill.component.ts
+++ angular/src/app/pages/bill/bill.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { param } from 'jquery';
 import { BillService } from 'src/app/services/bill/bill.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-bill',
@@ -28,7 +30,34 @@ export class BillComponent  implements OnInit{
     );
   }
 
-  huyDon(): void {
-    // Xử lý logic khi người dùng hủy đơn hàng
+  huyDon(orderId: string, status: string): void {
+    const updatedStatus = '6488a33d098b67f90d85df7a'; // Trạng thái mới là 'cancel'
+
+    Swal.fire({
+      title: 'Hủy đơn hàng',
+      text: 'Bạn có chắc muốn hủy đơn hàng này?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Đồng ý',
+      cancelButtonText: 'Hủy bỏ'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.orderService.updateBill({ _id: orderId, status: updatedStatus }).subscribe(
+          (response: any) => {
+            console.log(response);
+            Swal.fire('Thành công', 'Đã hủy đơn hàng', 'success');
+            this.getOrderDetails();
+          },
+          (error: any) => {
+            console.log('Error:', error);
+            Swal.fire('Lỗi', 'Đã xảy ra lỗi khi hủy đơn hàng', 'error');
+          }
+        );
+      }
+    });
   }
+
+
 }
